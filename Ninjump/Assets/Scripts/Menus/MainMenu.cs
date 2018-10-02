d01595bba41fb22c04d1cd88375d5eca6db90691
Christina Nita <cristinaflorinaeu@gmail.com>
2018-10-03 00:12:17 +0100

    Initial Commit

---
+++ Ninjump/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿// Code adapted from:
+// - https://www.youtube.com/watch?v=zc8ac_qUXQY&t=556s
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    	
+	public void PlayGame () {
+        SceneManager.LoadScene(1);
+	}
+
+    public void Levels()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+    }
+
+}
