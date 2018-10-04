305f7720f50e132741544ce31cf2793c1321ce12
Christina Nita <cristinaflorinaeu@gmail.com>
2018-10-04 22:40:44 +0100

    Automatic Movement

---
+++ Ninjump/Assets/Scripts/Ninja/Movement/GameManagerScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector3 moveVector;
+
+    private void Start()
+    {
+        moveSpeed = 100f;
+        moveVector.Set(1, 0, 0);
+    }
+}
---
+++ Ninjump/Assets/Scripts/Ninja/Movement/NinjaMoveAuto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaMoveAuto : MonoBehaviour {
+    GameManagerScript GameManager;
+
+	// Use this for initialization
+	void Start () {
+        GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
+        GameManager = gameController.GetComponent<GameManagerScript>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+//         Debug.Log(GameManager.moveSpeed);
+
+        transform.Translate(GameManager.moveVector * GameManager.moveSpeed * Time.deltaTime);
+	}
+}
